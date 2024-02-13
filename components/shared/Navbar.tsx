5121fd353cea8e4801e476a49058f5e32f24cc81
unknown <gagas112212@gmail.com>
2024-02-13 14:31:56 +0700

    fix:navbar bugs

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="flex space-x-6 justify-center items-center">
         <Link
           href={"/"}
-          className="font-bold text-lg hover:text-[#71717a] transition-all duration-300"
+          className="font-bold text-lg  px-2 py-1 rounded hover:bg-[#e6e8eb80] hover:text-[#5cc081] text-[#16a34a] transition-all duration-300"
         >
           CodingCraft
         </Link>
