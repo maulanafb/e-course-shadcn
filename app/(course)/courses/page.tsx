e41f051222de0537c8a0e3935f9fd27e77762eeb
unknown <gagas112212@gmail.com>
2024-02-13 13:56:29 +0700

    fix:quote error

--- app/(course)/courses/page.tsx
+++ app/(course)/courses/page.tsx
@@ -6,7 +6,7 @@ const Courses = () => {
     <section className="min-h-svh  mx-auto">
       <div className="mx-auto items-center flex flex-col py-10">
         <div>
-          <h1 className="text-title text-[100px]">Unlock Your Skill's</h1>
+          <h1 className="text-title text-[100px]">Unlock Your Skill&rsquo;s</h1>
         </div>
         <div className="flex py-10 max-w-[1240px]">All Courses</div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-8 mt-[20px] justify-center">
